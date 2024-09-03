79575e6fe49f153b4fffa3caf1dd48a6e276ccbd
luan ramos vicente <luanrv00@gmail.com>
2024-09-03 10:36:51 -0300

    customizes SearchForm with shadcnui

--- src/app/ui/SearchForm/index.tsx
+++ src/app/ui/SearchForm/index.tsx
@@ -1,10 +1,49 @@
+"use client";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Input,
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+} from "@/shadcn/ui";
+
+const formSchema = z.object({
+  item: z.string(),
+});
+
 export function SearchForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      item: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log("--------------- values", values);
+  }
+
   return (
-    <form>
-      <div className="flex flex-col">
-        <label>Search for a Anime, TV Serie or Movie</label>
-        <input type="text" placeholder="Type anything..." />
-      </div>
-    </form>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="item"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Search for an Anime, TV Serie or Movie</FormLabel>
+              <FormControl>
+                <Input {...field} placeholder="Type anything..." />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
   );
 }
